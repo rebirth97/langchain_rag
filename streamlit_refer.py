623e68805f79a81a89d890fdf2d817eec86d336f
rebirth97 <42992810+rebirth97@users.noreply.github.com>
2024-03-06 17:26:23 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -119,7 +119,7 @@ def get_text(docs):
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
+        chunk_size=500,
         chunk_overlap=100,
         length_function=tiktoken_len
     )
